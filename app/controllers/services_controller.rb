c5f6ba2efe585dca73120694e44dbf6955fde682
Shu-Yi Lim <shu.lim27@gmail.com>
2021-03-03 23:01:31 +0800

    added services routes, done services controller

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,48 @@
+class ServicesController < ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.valid?
+      @service.save
+      redirect_to services_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to service_path(@service)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+    redirect_to services_path
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :price, :category, :user_id)
+  end
+end
