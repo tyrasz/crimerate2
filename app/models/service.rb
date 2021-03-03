b92898b284a8249215bf91d8f1a1df54f35fb38a
Shu-Yi Lim <shu.lim27@gmail.com>
2021-03-04 01:01:52 +0800

    fixed validation, started on services view

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,7 +3,7 @@ class Service < ApplicationRecord
   has_many :jobs
   # has_one_attached :photo
 
-  validates :name
+  validates :name, presence: true
   validates :price, presence: true, numericality: true
   validates :category, presence: true, inclusion: { in: %w(Burglary Cybercrime Drugs Harassment Homicide Theft Vandalism) }
 end
