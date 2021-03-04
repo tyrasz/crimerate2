2e36d4c26386c353eb46053bae144c5ca7be702d
Wong Zhi Kai <wzhikai@gmail.com>
2021-03-05 00:18:37 +0800

    added containers for signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  #shorten login url from users/login to login
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  #shorten logout url from users/logout to logout
+  devise_scope :user do
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
