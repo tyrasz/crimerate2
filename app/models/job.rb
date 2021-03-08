169e56da2e6714625cdece57f7f6e162e8e3d0fd
Margarita Maltseva <margarita.maltseva@protonmail.com>
2021-03-08 22:04:55 +0800

    markers added to the map

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,5 +8,5 @@ class Job < ApplicationRecord
   has_one :review, dependent: :destroy
 
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
