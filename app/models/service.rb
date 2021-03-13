c0206fc69141db2a7080a4c195dd6c5e0b622f48
Shu-Yi Lim <shu.lim27@gmail.com>
2021-03-13 12:51:37 +0800

    added function for global search in navbar (service.name/category ; user.handle/location)

--- app/models/service.rb
+++ app/models/service.rb
@@ -7,6 +7,16 @@ class Service < ApplicationRecord
   validates :price, presence: true, numericality: true
   validates :category, presence: true, inclusion: { in: %w(Burglary Cybercrime Drugs Harassment Homicide Theft Vandalism) }
 
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :category ],
+    associated_against: {
+      user: [ :handle, :location ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
   # def self.search(search)
   #   if search
   #     where(["name ILIKE ?", "%#{search}%"])
