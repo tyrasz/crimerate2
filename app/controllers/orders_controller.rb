4509ca462618b2ff03a9db688aa64afcc4a8f8b5
hwajie <soh_hwajie@hotmail.com>
2021-03-13 13:32:50 +0800

    integrated stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   def create
     service = Service.find(params[:service_id])
     order = Order.create!(service: service, service_sku: service.name, amount: service.price, state: 'pending', user: current_user)
+    skip_authorization
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
@@ -17,5 +18,9 @@ class OrdersController < ApplicationController
 
     order.update(checkout_session_id: session.id)
     redirect_to new_order_payment_path(order)
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
   end
 end
