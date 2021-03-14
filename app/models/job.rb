3872b9edcb6515eaf954c19ac57244fee519fab1
Margarita Maltseva <margarita.maltseva@protonmail.com>
2021-03-14 20:13:38 +0800

    please add the menu for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,7 @@ class Job < ApplicationRecord
   belongs_to :service
   belongs_to :user
   has_one :review, dependent: :destroy
+  has_one_attached :photo
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
