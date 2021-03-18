1f966094bfac45ba5aa566c7eb499bc56dd42d3f
hwajie <soh_hwajie@hotmail.com>
2021-03-18 16:55:35 +0800

    didnt change much

--- config/routes.rb
+++ config/routes.rb
@@ -24,12 +24,6 @@ Rails.application.routes.draw do
     resources :jobs, only: [:new, :create, :edit, :update]
   end
 
-  # resources :services do
-  #   collection do
-  #     get :top
-  #   end
-  # end
-
   resources :users, only: [ :show ] #revert back
 
   # vendor route to find vendor
